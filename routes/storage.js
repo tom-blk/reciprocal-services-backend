a8c200ac47089114174d49be5a94b2ba2eadf6af
Tom Bleek <tom.bleek@proton.me>
2024-01-09 15:57:57 +0100

    fixed gitignore and unnecessary storage.js content

--- routes/storage.js
+++ routes/storage.js
@@ -14,21 +14,7 @@ const storageProfilePicture = multer.diskStorage({
     }
 })
 
-const storageServicePicture = multer.diskStorage({
-    destination: function (req, file, cb){ 
-        console.log(file);
-        cb(null, 'uploads/service-pictures');
-    },
-    filename: function (req, file, cb){
-        console.log(file);
-        cb(null, 'picture.png');
-    }
-})
-
 const uploadProfilePicture = multer({storage: storageProfilePicture})
-
-const uploadServicePicture = multer({storage: storageServicePicture})
-
 
 router.post('/upload-user-picture', uploadProfilePicture.single('picture'), (req, res) => {
     
@@ -39,17 +25,6 @@ router.post('/upload-user-picture', uploadProfilePicture.single('picture'), (req
             res.status(500).send("Error updating your profile picture.");
         }else{
             res.status(200).send("Profile picture successfully updated.");
-        }
-    })
-})
-
-router.post('/upload-service-picture', uploadServicePicture.single('picture'), (req, res) => {
-    
-    fs.rename('uploads/service-pictures/picture.png', `uploads/service-pictures/service-${req.body.serviceId}-service-picture.png`, (error) => {
-        if(error){
-            fs.unlink('uploads/service-pictures/picture.png', (error) => {console.log(error)})
-            console.log(error);
-            res.status(500).send("Error updating the service picture.")
         }
     })
 })
